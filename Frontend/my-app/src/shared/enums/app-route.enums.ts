4a60a67838811413ee44484db6923239eeec24e6
”Taras <taras.mavrovich1998@gmail.com>
2025-03-17 17:42:33 +0200

    taras delete my-app

--- Frontend/my-app/src/shared/enums/app-route.enums.ts
+++
@@ -1,7 +0,0 @@
-const AppRoute = {
-  ROOT: "/",
-  SIGN_IN: "/auth/sign-in",
-  SIGN_UP: "/auth/sign-up",
-} as const;
-
-export { AppRoute };
--- Frontend/my-app/src/shared/types/types.ts
+++
@@ -1,3 +0,0 @@
-import { ModalProps } from "./authTypes/authTypes";
-
-export type { ModalProps };
